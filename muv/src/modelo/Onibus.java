2f3f64c4ff7e9d46a32f15fdeaa52c8cc81366bd
Mandinha11 <amanda.al20@aluno.ifsc.edu.br>
2022-11-21 15:17:23 -0300

    Editei o código

--- muv/src/modelo/Onibus.java
+++ muv/src/modelo/Onibus.java
@@ -2,9 +2,52 @@ package modelo;
 
 public class Onibus extends Veiculo {
 	
-	public int assento;
+	public int assentos;
 	public int capacidade;
 	public int andares;
 	public boolean sanfonado;
+	
+	public Onibus(int assentos, int capacidade, int andares, boolean sanfonado, String marca, String modelo, String cor) {
+		super (marca, modelo, cor);
+		
+		this.assentos = assentos;
+		this.capacidade = capacidade;
+		this.andares = andares;
+		this.sanfonado = sanfonado;
+		
+		}
+	public int getassentos() {
+		return assentos;
+		}
+	
+	public void setassentos(int assentos) {
+		this.assentos = assentos;
+		}
+	
+	public int getcapacidade() {
+	return capacidade;
+		}
+	
+	public void setcapacidade(int capacidade) {
+	this.capacidade = capacidade;
+		}
+	
+	public int getandares() {
+	return andares;
+		}
+
+	public void setandares(int andares) {
+	this.andares = andares;
+		}
+	
+	public boolean getsanfonado() {
+		return sanfonado;
+		}
+
+		public void setsanfonado(boolean sanfonado) {
+		this.sanfonado = sanfonado;
+		}
 
 }
+
+
--- muv/src/modelo/Veiculo.java
+++ muv/src/modelo/Veiculo.java
@@ -6,8 +6,36 @@ public class Veiculo {
 	public String modelo;
 	public String cor;
 	
-	public Veiculo() {
+	public Veiculo(String marca, String modelo, String cor) {
+		this.marca = marca;
+		this.modelo = modelo; 
+		this.cor = cor;
 		
 	}
+	
+	public String getmarca() {
+		return marca;
+	}
+	
+	public void setmarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public String getmodelo() {
+		return modelo;
+	}
+	
+	public void setmodelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public String getcor() {
+		return cor;
+	}
+	
+	public void setcor(String cor) {
+		this.cor = cor;
+	}
+	
 
 }
