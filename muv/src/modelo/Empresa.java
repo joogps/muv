f842e3f09522ed27c1ee45a2e914590cfaab8d18
Erico26 <eric.o26@aluno.ifsc.edu.br>
2022-11-21 14:43:46 -0300

    Update Empresa.java

--- muv/src/modelo/Empresa.java
+++ muv/src/modelo/Empresa.java
@@ -1,5 +1,51 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Empresa {
 
+	public String nome;
+	public String cnpj;
+	public ArrayList<Linha> linhas;
+	public ArrayList<Veiculo> frota;
+	
+	public Empresa(String nome, String cnpj, ArrayList<Linha> linhas, ArrayList<Veiculo> frota) {
+		this.nome = nome;
+		this.cnpj = cnpj;
+		this.linhas = linhas;
+		this.frota = frota;
+		
+	}
+	
+	public String getnome() {
+		return nome;
+	}
+	
+	public void setnome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getcnpj() {
+		return cnpj;
+	}
+	
+	public void setcnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	public ArrayList<Linha> getlinhas(){
+		return linhas;
+	}
+	
+	public void setlinhas(ArrayList<Linha> linhas) {
+		this.linhas = linhas;
+	}
+	
+	public ArrayList<Veiculo> getfrota(){
+		return frota;
+	}
+	
+	public void setfrota(ArrayList<Veiculo> frota) {
+		this.frota = frota;
+	}
 }
